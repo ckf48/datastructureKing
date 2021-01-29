335fcef2118469cd3e36caeb0c87762f0c330a79
ckf48 <3048764886@qq.com>
2021-01-29 19:06:50 +0800

    顺序表，前三题

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include "liner_list/Exercise1.h"
+using namespace std;
+
+void test_liner_list_exercise1(){
+    int A[] = {7,2,3,4,5};
+    int value = Exercise1::exercise1(A, 5);
+    cout << value << " " << A[1] << endl;
+}
+
+void test_liner_list_exercise2(){
+    int A[] = {1,2,3,4,5};
+    Exercise1::exercise2(A,5);
+    for(int i : A){
+        cout<< i << " ";
+    }
+}
+
+void test_liner_list_exercise3(){
+    int A[] = {1,1,3,4,5,6,1,1,7,1,8};
+    Exercise1::exercise3(A,1,11);
+    for(int i : A){
+        cout<< i << " ";
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    /*1.1*/
+    //test_liner_list_exercise1();
+
+    /*1.2*/
+    //test_liner_list_exercise2();
+
+    /*1.3*/
+    //test_liner_list_exercise3();
+
     return 0;
 }
+
+
