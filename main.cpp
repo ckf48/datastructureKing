513097326209bfa293efe0c9de27e8fdaa178f20
ckf48 <3048764886@qq.com>
2021-01-30 19:16:35 +0800

    顺序表，4-7题

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 #include "liner_list/Exercise1.h"
 using namespace std;
 
-void test_liner_list_exercise1(){
+void test_liner_list_exercise1_1(){
     int A[] = {7,2,3,4,5};
     int value = Exercise1::exercise1(A, 5);
     cout << value << " " << A[1] << endl;
 }
 
-void test_liner_list_exercise2(){
+void test_liner_list_exercise1_2(){
     int A[] = {1,2,3,4,5};
     Exercise1::exercise2(A,5);
     for(int i : A){
@@ -16,7 +16,7 @@ void test_liner_list_exercise2(){
     }
 }
 
-void test_liner_list_exercise3(){
+void test_liner_list_exercise1_3(){
     int A[] = {1,1,3,4,5,6,1,1,7,1,8};
     Exercise1::exercise3(A,1,11);
     for(int i : A){
@@ -24,16 +24,61 @@ void test_liner_list_exercise3(){
     }
 }
 
+void test_liner_list_exercise1_4(){
+    int A[] = {1,2,3,4,5,6,7,8,9,10};
+    Exercise1::exercise4(A,4,7,10);
+    for(int i : A){
+        cout<< i << " ";
+    }
+}
+
+void test_liner_list_exercise1_5(){
+    int A[] = {11,2,13,4,5,61,7,8,9,10};
+    Exercise1::exercise5(A,2,9,10);
+    for(int i : A){
+        cout<< i << " ";
+    }
+}
+
+void test_liner_list_exercise1_6(){
+    int A[] = {1,1,2,3,3,3,4,4,5,5};
+    Exercise1::exercise6(A,10);
+    for(int i : A){
+        cout<< i << " ";
+    }
+}
+
+void test_liner_list_exercise1_7(){
+    int A[] = {1,2,3,4,5};
+    int B[] = {6,7,8,9,10};
+    int* C = Exercise1::exercise7(A,B,5,5);
+    for(int i = 0; i < 10; i++){
+        cout<< C[i] << " ";
+    }
+    delete [] C;
+}
+
 int main() {
     /*1.1*/
-    //test_liner_list_exercise1();
+    //test_liner_list_exercise1_1();
 
     /*1.2*/
-    //test_liner_list_exercise2();
+    //test_liner_list_exercise1_2();
 
     /*1.3*/
-    //test_liner_list_exercise3();
+    //test_liner_list_exercise1_3();
 
+    /*1.4*/
+    //test_liner_list_exercise1_4();
+
+    /*1.5*/
+    //test_liner_list_exercise1_5();
+
+    /*1.6*/
+    //test_liner_list_exercise1_6();
+
+    /*1.7*/
+    test_liner_list_exercise1_7();
     return 0;
 }
 
